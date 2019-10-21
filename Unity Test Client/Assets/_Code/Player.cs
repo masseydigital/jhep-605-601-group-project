43b32401011a80245947ed104a8c9dbf3461e551
masseydigital <acemgaming@gmail.com>
2019-10-20 22:19:34 -0400

    Adding networking updates.  Modified server app to display messages.

--- Unity Test Client/Assets/_Code/Player.cs
+++ Unity Test Client/Assets/_Code/Player.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.Networking.PlayerConnection;
+using UnityEngine.Networking.Types;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isServer)
+        {
+            Debug.Log(":: I AM SERVER ::");
+        }
+
+        if (isClient)
+        {
+            Debug.Log($":: I AM CLIENT: {GetComponent<NetworkIdentity>().playerControllerId} ::");
+        }
     }
 
     // Update is called once per frame
