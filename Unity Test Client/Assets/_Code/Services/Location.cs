6ea0c7b961e5b9c5cdf78bed5d65683df7d0dbd2
Luc <neyugncul@gmail.com>
2019-10-21 15:49:11 -0400

    adding location

--- Unity Test Client/Assets/_Code/Services/Location.cs
+++ Unity Test Client/Assets/_Code/Services/Location.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Location : MonoBehaviour
+ class Location : MonoBehaviour
 {
-    public List<string> rowNames;
+    public List<Position> positions = new List<Position>();
+    public const int maxRow = 3;
+    public const int maxColumn = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -17,4 +19,69 @@ public class Location : MonoBehaviour
     {
         
     }
+
+    // Returns True if successfully used secret passage; false otherwise
+    public bool useSecretPassage(Player player)
+    {
+        
+        foreach (Position pos in positions)
+        {
+            if(pos.player == player)
+            {
+                // TODO: check diagonals and see if there's an open position
+            }
+        }
+
+        return true;
+    }
+
+
+    public class Position
+    {
+        public int row;
+        public int column;
+        public string locationName;
+        public Player player;
+
+        /*public int getRow()
+        {
+            return row;
+        }
+
+        public int getColumn()
+        {
+            return column;
+        }
+
+        public string getLocationName()
+        {
+            return locationName;
+        }
+
+        private Player getPlayer()
+        {
+            return player;
+        }
+
+        private void setRow(int newRow)
+        {
+            row = newRow;
+        }
+
+        private void setColumn(int newColumn)
+        {
+            column = newColumn;
+        }
+
+        private void setLocationName(string newLocationName)
+        {
+            locationName = newLocationName;
+        }
+
+        private void setPlayer(Player newPlayer)
+        {
+            player = newPlayer;
+        }*/
+    }
+
 }
