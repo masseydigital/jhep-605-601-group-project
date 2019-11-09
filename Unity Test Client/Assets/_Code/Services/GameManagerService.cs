be0f926b824501f6f2357ea58e03cacf609b89a7
King Carmichael III <kc3apps@gmail.com>
2019-11-09 11:35:54 -0500

    Added game manager service and case file class

---
+++ Unity Test Client/Assets/_Code/Services/GameManagerService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerService : MonoBehaviour
+{
+    public List<Player> players;
+    public Player playerTurn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool checkWinner(Player player)
+    {
+        if (player.winLoose == 1) return true;
+        else return false;
+    }
+
+    public bool checkLoser(Player player)
+    {
+        if (player.winLoose != 1) return true;
+        else return false;
+    }
+}
